da75273ed574a51fb518ef3e20e49bfb35c24444
Camila <milabs.esp24@gmail.com>
2026-05-14 10:53:13 +0200

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,15 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
+  customWorkerSrc: 'sw-custom.js',
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   disable: process.env.NODE_ENV === 'development',
   workboxOptions: {
     disableDevLogs: true,
+    skipWaiting: true, // ← aquí
+    clientsClaim: true, // ← aquí
   },
 });
 
